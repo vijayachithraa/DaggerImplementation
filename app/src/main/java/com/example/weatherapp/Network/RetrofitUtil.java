57dca9d6b047faf1c87e05b1137871355216004c
vijayachithraa <vijayachithraa>
2019-07-19 16:01:40 +0530

    DaggerRXJavaupdatedTest

--- app/src/main/java/com/example/weatherapp/Network/RetrofitUtil.java
+++ app/src/main/java/com/example/weatherapp/Network/RetrofitUtil.java
@@ -1,5 +1,6 @@
 package com.example.weatherapp.Network;
 
+import dagger.Provides;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
---
+++ app/src/main/java/com/example/weatherapp/Network/SampleTest.java
@@ -0,0 +1,4 @@
+package com.example.weatherapp.Network;
+
+public class SampleTest {
+}
